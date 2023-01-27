55080c3effd2b090b1338a1166016d7b7af9f842
Natallia2107 <105502657+Natallia2107@users.noreply.github.com>
2023-01-27 16:12:26 +0100

    create first component folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
+import { CTA, Brand, Navbar } from './components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className='App'>
+			<div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
+
+// rafce = tworzy to :
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+// export default App
