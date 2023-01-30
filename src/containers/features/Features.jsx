88a3f51e3d1fb82fb5abd99b0f383accdb55f119
Natallia2107 <105502657+Natallia2107@users.noreply.github.com>
2023-01-30 17:35:13 +0100

    created feature component and fix whatgpt3

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import './features.css'
 
 const Features = () => {
-  return (
-    <div>
-      Features
-    </div>
-  )
+	return (
+		<div>
+			featuressssss
+		</div>
+	)
 }
 
 export default Features
